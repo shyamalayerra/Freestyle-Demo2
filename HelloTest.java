dde075c2d3aa010fdebda61102624c8392a6a9db
shyamalayerra <72130097+shyamalayerra@users.noreply.github.com>
2026-02-12 22:04:33 +0530

    Create  HelloTest.java

---
+++ HelloTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testHello() {
+        assertEquals("Hello World", "Hello World");
+    }
+}
